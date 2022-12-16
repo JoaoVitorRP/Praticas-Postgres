0b4ea9eebeb9d15427e7fb7700da50da21d45ce6
JoaoVitorRP <joaovrp.mec.ufsc@gmail.com>
2022-12-16 14:34:06 -0300

    Pratica - Exercicios Group By

---
+++ Pratica - Exercicios Group By/03.sql
@@ -0,0 +1,1 @@
+SELECT u.name AS writer, COUNT(t."writerId") AS "testimonialCount" FROM testimonials t JOIN users u ON t."writerId" = u.id WHERE t."writerId" = 435 GROUP BY u.id;
---
+++ Pratica - Exercicios Group By/04.sql
@@ -0,0 +1,1 @@
+SELECT MAX(j.salary) AS "maximumSalary", r.name AS role FROM jobs j JOIN roles r ON j."roleId" = r.id WHERE j.active = true GROUP BY r.id ORDER BY "maximumSalary";
